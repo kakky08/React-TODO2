43e6d8c4cb7b54767c77231f2f280cd7e7c8eed8
kakky08 <77216691+kakky08@users.noreply.github.com>
2021-02-16 14:36:50 +0900

    HTMLの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import React from "react";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="inputArea">
+        <input placeholder="Todoを入力" className="input" />
+        <button className="inputButton">追加</button>
+      </div>
+      <div className="incompleteArea">
+        <p className="incompleteTitle">未完了なTODO</p>
+        <div className="incomplete">
+          <ul className="incompleteList">
+            <li className="incompleteListItem">ああああ</li>
+            <button className="incompleteButton">進行中</button>
+            <button className="incompleteButton">削除</button>
+          </ul>
+        </div>
+        <div className="incompleteArea">
+          <p className="incompleteTitle">未完了なTODO</p>
+          <div className="incomplete">
+            <ul className="incompleteList">
+              <li className="incompleteListItem">いいいい</li>
+              <button className="incompleteButton">進行中</button>
+              <button className="incompleteButton">削除</button>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="inProgressArea">
+        <p className="inProgressTitle">進行中のTODO</p>
+        <div className="inProgress">
+          <ul className="inProgressList">
+            <li className="inProgressListItem">うううう</li>
+            <button className="inProgressButton">完了</button>
+            <button className="inProgressButton">戻す</button>
+          </ul>
+        </div>
+      </div>
+      <div className="completeArea">
+        <p className="completeTitle">完了したTODO</p>
+        <div className="complete">
+          <ul className="completeList">
+            <li className="completeListItem">ええええ</li>
+            <button className="completeButton">戻す</button>
+            <button className="completeButton">削除</button>
+          </ul>
+        </div>
+      </div>
+    </>
   );
-}
+};
