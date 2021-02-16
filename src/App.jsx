3250073a4e48521ebcb07a5cea7810a23a0ba473
kakky08 <77216691+kakky08@users.noreply.github.com>
2021-02-16 16:52:01 +0900

    最終調整

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { ToDo } from "./components/ToDo";
 import { Incomplete } from "./components/Incomplete";
