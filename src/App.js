2489f03093cf5ab1bdd9700fad36efa17fce014e
kakky08 <77216691+kakky08@users.noreply.github.com>
2021-02-16 15:11:22 +0900

    HTML,CSSの変更

--- src/App.js
+++ src/App.js
@@ -4,49 +4,45 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div className="inputArea">
-        <input placeholder="Todoを入力" className="input" />
-        <button className="inputButton">追加</button>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
       </div>
-      <div className="incompleteArea">
-        <p className="incompleteTitle">未完了なTODO</p>
-        <div className="incomplete">
-          <ul className="incompleteList">
-            <li className="incompleteListItem">ああああ</li>
-            <button className="incompleteButton">進行中</button>
-            <button className="incompleteButton">削除</button>
-          </ul>
-        </div>
-        <div className="incompleteArea">
-          <p className="incompleteTitle">未完了なTODO</p>
-          <div className="incomplete">
-            <ul className="incompleteList">
-              <li className="incompleteListItem">いいいい</li>
-              <button className="incompleteButton">進行中</button>
-              <button className="incompleteButton">削除</button>
-            </ul>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>進行中</button>
+            <button>削除</button>
           </div>
-        </div>
+
+          <div className="list-row">
+            <li>いいいい</li>
+            <button>進行中</button>
+            <button>削除</button>
+          </div>
+        </ul>
       </div>
-      <div className="inProgressArea">
-        <p className="inProgressTitle">進行中のTODO</p>
-        <div className="inProgress">
-          <ul className="inProgressList">
-            <li className="inProgressListItem">うううう</li>
-            <button className="inProgressButton">完了</button>
-            <button className="inProgressButton">戻す</button>
-          </ul>
-        </div>
+      <div className="inProgress-area">
+        <p className="title">進行中のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>うううう</li>
+            <button>完了</button>
+            <button>戻す</button>
+          </div>
+        </ul>
       </div>
-      <div className="completeArea">
-        <p className="completeTitle">完了したTODO</p>
-        <div className="complete">
-          <ul className="completeList">
-            <li className="completeListItem">ええええ</li>
-            <button className="completeButton">戻す</button>
-            <button className="completeButton">削除</button>
-          </ul>
-        </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ええええ</li>
+            <button>戻す</button>
+            <button>削除</button>
+          </div>
+        </ul>
       </div>
     </>
   );
